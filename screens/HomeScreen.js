5f45d9d04769b5ab38b35297b4526a6471f97c47
Arjun <rao.arjun87@gmail.com>
2017-06-28 23:44:15 -0400

    Code cleanup - Homescreen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import {
-  Image,
-  Linking,
   Platform,
-  ScrollView,
   StyleSheet,
-  SliderIOS,
   Text,
-  TouchableOpacity,
   TouchableHighlight,
   View,
-  Button,
-  Alert,
-  ListView,
 } from 'react-native';
 
 var requests = require('../requests/requests');
@@ -47,7 +39,7 @@ export default class HomeScreen extends React.Component {
       </View>
     );
   }
-  
+
   getFoodOption = () => {
     requests.getFoodOptionAsync(1,2,3);
   };
@@ -76,118 +68,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     textAlign: 'center',
     fontFamily: 'Iowan Old Style',
-  },
-  resultContainer: {
-    flex: 2,
-    backgroundColor: '#BBBBBB',
-  },
-  optionsContainer: {
-    flex: 3,
-    backgroundColor: '#000000',
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 15,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 80,
-  },
-  welcomeContainer: {
-    alignItems: 'center',
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  welcomeImage: {
-    width: 140,
-    height: 38,
-    resizeMode: 'contain',
-    marginTop: 3,
-    marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 23,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
-  },
-  slider: {
-    height: 10,
-    margin: 10,
-  },
-  text: {
-    fontSize: 14,
-    textAlign: 'center',
-    fontWeight: '500',
-    margin: 10,
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
   },
 });
