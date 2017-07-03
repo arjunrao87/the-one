8438894be7b1bfc0f2a4d24db17cc7c3622eb5f8
Arjun <rao.arjun87@gmail.com>
2017-07-03 15:41:59 -0400

    Adding slider bar to menu

--- components/Menu.js
+++ components/Menu.js
@@ -7,43 +7,88 @@ import {
   Text,
   TouchableHighlight,
   TouchableOpacity,
+  Slider,
   View,
 } from 'react-native'
-var Slider = require('react-native-slider');
 
 export default class Menu extends React.Component {
 
   constructor( props ){
     super( props );
-    this.state = {value:2.0};
+    this.state = {distance:500,price:2, priceRating:'$$'};
   }
   render() {
     return (
       <View style={{flex:1}}>
-        <ScrollView style={{flex:1, backgroundColor:'cyan', padding:20}}>
-          <Text>Filters</Text>
-          <View>
-            <Text>Distance</Text>
-            <Text>Value: {this.state.value}</Text>
+        <View style={{flex:1, backgroundColor:'ivory', padding:20}}>
+          <View style={styles.distance} >
+            <Text style={styles.menuText}>Distance - {this.state.distance} m</Text>
+              <Slider
+               value={this.state.distance}
+               value= {500}
+               maximumValue={5000}
+               minimumValue={0}
+               onSlidingComplete={(value) => {
+                 console.log( "Finish Value = " + value);
+                 this.setState({distance:parseInt(value)})}
+                  }/>
           </View>
-          <View>
-            <Text>Price</Text>
+          <View style={styles.distance} >
+            <Text style={styles.menuText}>Price  - {this.state.priceRating} </Text>
+              <Slider
+               value={this.state.price}
+               maximumValue={4}
+               minimumValue={1}
+               step={1}
+               onSlidingComplete={(value) => {
+                 console.log( "Finish Value = " + value);
+                 this.setState({price:parseInt(value)})
+                 if( value == 1 ){
+                   this.setState( {priceRating:'$'});
+                 } else if ( value == 2 ){
+                   this.setState( {priceRating:'$$'});
+                 } else if ( value == 3 ){
+                   this.setState( {priceRating:'$$$'});
+                 } else if ( value == 4 ){
+                   this.setState( {priceRating:'$$$$'});
+                 }
+                }
+              }/>
           </View>
-          <View>
-            <Text>Freeze for 30 mins</Text>
-          </View>
-          <Text>Help</Text>
-          <Text>About</Text>
-        </ScrollView>
-        <View style={{flex:1}}>
+          <TouchableHighlight style={{flex:1}} >
+            <Text style={styles.menuText}>Help</Text>
+          </TouchableHighlight>
+          <TouchableHighlight style={{flex:1}} >
+            <Text style={styles.menuText}>About</Text>
+          </TouchableHighlight>
+        </View>
+        <View style={{flex:1, backgroundColor:'ivory'}}>
         </View>
       </View>
     )
+  }
+
+  complete(){
+    console.log( )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    padding: 40,
+  },
+  menuText:{
+    fontSize: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    textAlign: 'center',
+    fontFamily:'Avenir'
+  },
+  distance: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 40,
+    alignItems: 'stretch',
+    justifyContent: 'center',
   },
 })
