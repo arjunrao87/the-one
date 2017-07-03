5465decc70041f0e6faa0f5b4fdc126ae7b9f16f
Arjun <rao.arjun87@gmail.com>
2017-07-03 12:24:49 -0400

    Adding details to the menu

--- components/Menu.js
+++ components/Menu.js
@@ -5,24 +5,45 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableHighlight,
   TouchableOpacity,
   View,
 } from 'react-native'
+var Slider = require('react-native-slider');
 
 export default class Menu extends React.Component {
 
+  constructor( props ){
+    super( props );
+    this.state = {value:2.0};
+  }
   render() {
     return (
-      <ScrollView style={styles.container}>
-      </ScrollView>
+      <View style={{flex:1}}>
+        <ScrollView style={{flex:1, backgroundColor:'cyan', padding:20}}>
+          <Text>Filters</Text>
+          <View>
+            <Text>Distance</Text>
+            <Text>Value: {this.state.value}</Text>
+          </View>
+          <View>
+            <Text>Price</Text>
+          </View>
+          <View>
+            <Text>Freeze for 30 mins</Text>
+          </View>
+          <Text>Help</Text>
+          <Text>About</Text>
+        </ScrollView>
+        <View style={{flex:1}}>
+        </View>
+      </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     padding: 20,
-    backgroundColor: 'lightcyan',
   },
 })
