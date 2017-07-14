fa2c8c4de62adea2edaba8e2eff60701227c6ce7
Arjun <rao.arjun87@gmail.com>
2017-07-13 22:59:36 -0400

    1. Docker + PM2 2. Menu icon remains hamburger with custom hamburger menu 3. Indicate pressed button with underline 4. Remove border, remove color of pressed button

--- components/App.js
+++ components/App.js
@@ -9,8 +9,8 @@ import {
 import Header from './Header';
 import {Permissions,Location} from 'expo';
 
-var baseURL = "http://localhost:8080/"
-              //  "http://104.236.66.151:8080/"
+var baseURL = //"http://localhost:8080/"
+                "http://104.236.66.151:8080/"
 
 export default class App extends React.Component {
 
